b13370b61ba00e48d7cb6815d5f60cb9e334bd7c
yannick <yannick.sealy08@gmail.com>
2016-03-14 00:31:50 +0000

    remove unnecessary templates and routes

--- src/clj/shorelands_api/handler.clj
+++ src/clj/shorelands_api/handler.clj
@@ -1,7 +1,6 @@
 (ns shorelands-api.handler
   (:require [compojure.core :refer [routes wrap-routes]]
             [shorelands-api.layout :refer [error-page]]
-            [shorelands-api.routes.home :refer [home-routes]]
             [shorelands-api.routes.services :refer [service-apis]]
             [compojure.route :as route]
             [shorelands-api.middleware :as middleware]))
@@ -10,7 +9,6 @@
 (def app-routes
   (routes
     #'service-apis
-    (wrap-routes #'home-routes middleware/wrap-csrf)
     (route/not-found nil)))
 
 (def app (middleware/wrap-base #'app-routes))
